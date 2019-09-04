22a6407020d6278dcf1960bab6ca58f47708e6b7
jvalenzuelaf <jvalenzuelaf@github.com>
2019-09-04 07:05:08 -0700

    Initial commit

---
+++ app/src/main/java/com/acme/dotacme/Default0.java
@@ -0,0 +1,14 @@
+package com.acme.dotacme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Default0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_default0);
+    }
+}
